6356a022318955868a622997a0b695b7d81dc8fa
tobeysnake <72365795+ebrahim575@users.noreply.github.com>
2022-01-16 22:18:29 -0600

    Update trim_mp3.py

--- trim_mp3.py
+++ trim_mp3.py
@@ -12,7 +12,7 @@ start = input('Enter the time you would like to start the mp3 : ')
 end = input('Enter the time you would like to end the mp3 : ')
 filename = input('Enter the file name : ')
 start = str(int(start.split(':')[0])*60 + int(start.split(':')[1]))
-end   = str(int(end.split(':')[0])*60 + int(end.split(':')[1]))
+end = str(int(end.split(':')[0])*60 + int(end.split(':')[1]))
 
 command = 'ffmpeg -ss ' + start + ' -t ' + end + ' -i ' + path + filename + ' -acodec copy ' + path + filename + '_'
 
