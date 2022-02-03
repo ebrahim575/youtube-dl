6386fe96a63114c66bdf9ab1607d1684afd56536
tobeysnake <72365795+ebrahim575@users.noreply.github.com>
2022-02-03 13:00:57 -0800

    intial commmit

--- rename.py
+++ rename.py
@@ -1,15 +1,4 @@
-
 import os
-folder = "/Users/ezulq/youtube-dl/"
-arr = os.listdir(path=folder)
-for i in range(len(arr)):
-    filename = arr[i]
-    
-    if '.mp3' in arr[i]:
-        filename = arr[i]
-        for j in range(len(filename)):
-            if not filename[j].isalpha():
-                filename[j].replace(filename[j],'')
 
-
-
+lst = os.listdir('/Users/ezulq/Desktop/')
+for i in range(len(lst)):
--- trim_mp3.py
+++ trim_mp3.py
@@ -1,4 +1,5 @@
 import os
+#import rename
 
 mac = 1
 windows = 0
@@ -8,9 +9,16 @@ if mac == 1:
 if windows == 1:
     path = 'C:\\Users\\ezulq\\youtube-dl\\'
 
+print('Renaming files.')
+#rename.main()
+print('Files renamed.')
+
 start = input('Enter the time you would like to start the mp3 : ')
 end = input('Enter the time you would like to end the mp3 : ')
 filename = input('Enter the file name : ')
+filename = rename.removenonalpha(filename)
+filename = filename[:len(filename)-4] + '.mp3'
+print(filename)
 start = str(int(start.split(':')[0])*60 + int(start.split(':')[1]))
 end   = str(int(end.split(':')[0])*60 + int(end.split(':')[1]))
 
